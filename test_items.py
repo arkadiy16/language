9a308751e1442f0ed8c367567f0959d31a59a86f
arkadiy16 <arkadiy160702@gmail.com>
2024-03-23 02:30:54 +0300

    Created conftest.py for test configurations and test_items for autotest.

---
+++ test_items.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+
+link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    # Wait to see page language
+    time.sleep(5)
+    try:
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.btn-add-to-basket')))
+        found = True
+    except:
+        found = False
+
+    assert found, 'Element not found'
